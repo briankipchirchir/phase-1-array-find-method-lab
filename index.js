b7cb4ea65824fc9ed37bd7b6a4b335c26dc396ae
briankipchirchir <kbrianngetich@gmail.com>
2024-07-02 15:23:08 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    { year: "2012", result: "N/A"},
+    { year: "2011", result: "N/A"},
+    { year: "2010", result: "N/A"},
+    { year: "2009", result: "N/A"},
+    { year: "2008", result: "N/A"},
+    { year: "2007", result: "N/A"},
+    { year: "2006", result: "N/A"},
+    { year: "2005", result: "N/A"}
+]
+
+function superbowlWin(record) {
+    const superbowlyear=record.find(result=>record.result==="W")
+    if (superbowlyear){
+        console.log(superbowlWin);
+        return superbowlyear.year;
+
+    }
+    else {
+        return undefined;
+    }
+    
+}
+superbowlWin;
